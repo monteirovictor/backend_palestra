2353ebe4d22b861c63a7dab1cf448d59f7ab73e6
monteirovictor <victormonteiromaia95@gmail.com>
2021-09-17 10:25:18 -0300

    change

--- src/middlewares/PacienteValidation.js
+++ src/middlewares/PacienteValidation.js
@@ -1,16 +1,16 @@
-const PacienteModel = require('../model/PacienteModel');
+// const PacienteModel = require('../model/PacienteModel');
 
-const PacienteValidation = async(req,res,next)=>{
+// const PacienteValidation = async(req,res,next)=>{
 
-    const { nome,
-    idade,
-    imc,
-    perc_gordura,
-    massa_magra,
-    massa_gorda,
-    valor_consulta,
-    when } = req.body;
+//     const { nome,
+//     idade,
+//     imc,
+//     perc_gordura,
+//     massa_magra,
+//     massa_gorda,
+//     valor_consulta,
+//     when } = req.body;
 
-}
+// }
 
-module.exports = PacienteValidation;
+// module.exports = PacienteValidation;
--- src/model/PacienteModel.js
+++ src/model/PacienteModel.js
@@ -4,14 +4,8 @@ const Schema = mongoose.Schema;
 const PacienteSchema = new Schema({
 
     nome:{type:String,required:true},
-    idade:{type:Number,required:true},
-    imc:{type:Number,required:true},
-    perc_gordura:{type:Number,required:true},
-    massa_magra:{type:Number,required:true},
-    massa_gorda:{type:Number,require:true},
-    valor_consulta:{type:Number,required:true},
-    when:{type:Date,required:true},
-    created:{type:Date, default:Date.now()}
+    idade:{type:Number,required:true}
+    
     
 
 });
